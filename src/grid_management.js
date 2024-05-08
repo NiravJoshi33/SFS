b152d2e4c84e3b8af1d06ee7d72646087af189f2
Nirav Joshi <niravjoshi3000@gmail.com>
2024-05-08 11:22:28 +0530

    added type annotation & comments

--- src/grid_management.js
+++ src/grid_management.js
@@ -3,11 +3,13 @@ import { enableSwap } from "./swap";
 
 /**
  * Instantiate a new grid without pre-existing matches & enable swap functionality
- * @param {Scene} scene
- * @param {any} swap_music
+ * @param {Scene} scene Game scene
+ * @param {Phaser.Sound.WebAudioSound} swap_music Swap sound effect
  */
 export function initiateGrid(scene, swap_music) {
+  // Create a new grid for the scene
   let grid = createGrid(scene);
 
+  // Enable swap functionality
   enableSwap(scene, grid, swap_music);
 }
