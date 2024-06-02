5b919c9837a1601825f8c56e7c78117d7094236e
Nirav Joshi <niravjoshi3000@gmail.com>
2024-06-02 10:10:15 +0530

    setup main.ts

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,19 +1,17 @@
-import Phaser from 'phaser'
+import Phaser from "phaser";
+import { canvasSize } from "./utils/gameConfig";
 
-import HelloWorldScene from './HelloWorldScene'
+// defining game configuration
+const config = {
+  type: Phaser.AUTO, // Phaser will decide how to render our game (WebGL or Canvas)
+  width: canvasSize.width,
+  height: canvasSize.height,
+  scene: [],
+  scale: {
+    mode: Phaser.Scale.FIT, // Fit the game to the window
+    autoCenter: Phaser.Scale.CENTER_BOTH, // Center the game on the window
+  },
+};
 
-const config: Phaser.Types.Core.GameConfig = {
-	type: Phaser.AUTO,
-	parent: 'app',
-	width: 800,
-	height: 600,
-	physics: {
-		default: 'arcade',
-		arcade: {
-			gravity: { y: 200 },
-		},
-	},
-	scene: [HelloWorldScene],
-}
-
-export default new Phaser.Game(config)
+// creating a new game instance
+const game = new Phaser.Game(config);
