10ca2699228cc882558108f9af82c4bd4a9f00b3
Nirav Joshi <niravjoshi3000@gmail.com>
2024-06-19 14:04:04 +0530

    deleted blueprint

--- token-smash/client/blueprint/wrappers/NftCollection.compile.ts
+++
@@ -1,9 +0,0 @@
-import { CompilerConfig } from '@ton/blueprint';
-
-export const compile: CompilerConfig = {
-    lang: 'tact',
-    target: 'contracts/nft_collection.tact',
-    options: {
-        debug: true,
-    },
-};
