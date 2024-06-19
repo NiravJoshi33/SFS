88c8868e38215d48aa1ec4bc29683caee30686c5
Nirav Joshi <niravjoshi3000@gmail.com>
2024-06-19 14:23:23 +0530

    updated configs to ignore token-smash folder

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,30 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  webpack: (config, { isServer }) => {
+    // Exclude the folder from the client-side build
+    if (!isServer) {
+      config.module.rules.push({
+        test: /\.(js|jsx|ts|tsx|mjs)$/,
+        include: [/ignored-folder/],
+        use: {
+          loader: "null-loader",
+        },
+      });
+    }
+
+    // You might also want to exclude the folder from the server-side build
+    if (isServer) {
+      config.externals = config.externals || [];
+      config.externals.push(function ({ context, request }, callback) {
+        if (/ignored-folder/.test(request)) {
+          return callback(null, "commonjs " + request);
+        }
+        callback();
+      });
+    }
+
+    return config;
+  },
+};
 
 export default nextConfig;
--- vite.config.ts
+++ vite.config.ts
@@ -11,13 +11,17 @@
 //   ],
 // })
 
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import { nodePolyfills } from 'vite-plugin-node-polyfills';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { nodePolyfills } from "vite-plugin-node-polyfills";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), nodePolyfills()],
-  base: '/',
+  base: "/",
+  build: {
+    rollupOptions: {
+      external: [/ignored-folder/],
+    },
+  },
 });
-
